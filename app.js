bc42662f47b3706598558ea21cadb78fd65bcadd
Jay Pick <jaypick@Jays-iMac.home>
2016-03-29 15:35:42 +0100

    [initial commit]

---
+++ app.js
@@ -0,0 +1,50 @@
+(function($, document) {
+
+   var thinklab = {
+
+      cache: function() {
+         thinklab.els = {};
+         thinklab.vars = {};
+
+         thinklab.els.$body = $('body');
+         thinklab.els.$window = $(window);
+         thinklab.els.$document = $(document);
+
+         thinklab.vars.$cookieNotice = $('#cookieNotice');
+      },
+
+      on_ready: function() {
+         thinklab.cache();
+         thinklab.on_load();
+      },
+
+      on_load: function() {
+         thinklab.els.$window.load( function() {
+            thinklab.showCookieNotice();
+            thinklab.addCookie();
+         });
+      },
+
+      showCookieNotice: function() {
+         if(Cookies.get('thinklabCookieNotice')) {
+            thinklab.vars.$cookieNotice.hide();
+         } else {
+            thinklab.vars.$cookieNotice.show();
+         }
+      },
+
+      addCookie: function() {
+         var btn = $('#addCookie');
+
+         btn.on('click', function() {
+            Cookies.set('thinklabCookieNotice', 'true', { expires: 7 });
+            thinklab.vars.$cookieNotice.hide();
+            return false;
+         });
+      },
+
+   };
+
+	$(document).ready(thinklab.on_ready());
+
+}(jQuery, document));
---
+++ readme.md
@@ -0,0 +1,4 @@
+<h1>Simple web cookie notice</h1>
+<p>This module is designed to be ported to your website for quick and easy integration. The cookie functionality is handled by a combination of <a href="https://github.com/js-cookie/js-cookie">js-cookie</a> and two jQuery functions.</p>
+<p>It's completely bare-bones, so be sure to add your own styles/transitions.</p>
+<p><i>Everyone loves a good cookie notice, right?</i></p>
